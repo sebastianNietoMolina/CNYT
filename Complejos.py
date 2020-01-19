2d4087f0204b7bb818b19ad8d2103e6906b00dcf
sebastianNietoMolina <juan.nieto-mo@mail.escuelaing.edu.co>
2020-01-19 10:46:53 -0500

    Calculadora con pruebas

--- Complejos.py
+++ Complejos.py
@@ -1,47 +1,110 @@
 import math
 
-def imprimir(ente,imag):
-    if imag<0:
-        res=str(ente)+str(imag)+'i'
-    elif imag==0:
-        res=str(ente)
-    else:
-        res=str(ente)+'+'+str(imag)+'i'
-    return res
-
 def sumar(a,b):
+    """Resuelve la suma de 2 numeros complejos.
+
+    Devuelve una tupla con la parte real y la parte imaginaria.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+    b -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     ente=a[0]+b[0]
     imag=a[1]+b[1]
     return (ente,imag)    
 
 def restar(a,b):
+    """Resuelve la resta de 2 numeros complejos.
+
+    Devuelve una tupla con la parte real y la parte imaginaria.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+    b -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     ente=a[0]-b[0]
     imag=a[1]-b[1]
     return (ente,imag)    
 
 def multiplicar(a,b):
+    """Resuelve la multiplicacion de 2 numeros complejos.
+
+    Devuelve una tupla con la parte real y la parte imaginaria.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+    b -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     ente=a[0]*b[0]+(b[1]*a[1])*-1
     imag=a[0]*b[1]+a[1]*b[0]
     return (ente,imag)    
 
 def modulo(a):
+    """Resuelve el modulo de un numero complejo.
+
+    Devuelve una tupla de un real.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     return (math.sqrt((a[0]**2+a[1]**2)))
     
 def division(a,b):
+    """Resuelve la division de 2 numeros complejos.
+
+    Devuelve una tupla con la parte real y la parte imaginaria.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+    b -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     div=b[0]**2+b[1]**2
     ente=(a[0]*b[0]+a[1]*b[1])/div
     imag=(a[1]*b[0]-a[0]*b[1])/div
     return (ente,imag)    
 
 def conjugado(a):
+    """Resuelve el conugado de un numero complejo.
+
+    Devuelve una tupla con la parte real y la parte imaginaria.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
     ente=a[0]
     imag=a[1]*-1
     return (ente,imag)
 
-def cartesianoPolar(a,b):
-    p=(math.sqrt(a**2+b**2))
-    o=(math.atan(b/a))
+def cartesianoPolar(a):
+    """Cambia de coordenadas cartesianas a polares de un numero complejo.
+
+    Devuelve una tupla con la parte real y un angulo.
+
+    Parametros:
+    a -- Es una tupla que contiene parte real y la parte imaginaria del numero complejo.
+
+    """
+    p=(math.sqrt(a[0]**2+a[1]**2))
+    o=(math.atan(a[1]/a[0]))
     return (p,o)
-    
+
+def polarCartesiano(p):
+    """Cambia de coordenadas polares a cartesianas de un numero complejo.
+
+    Devuelve una tupla con la parte real y la parte imaginaria del numero complejo.
+
+    Parametros:
+    p -- Es una tupla que contiene parte real y un angulo.
+
+    """
+    a=round(p[0]*math.cos(p[1]),3)
+    b=round(p[0]*math.sin(p[1]),3)
+    return (a,b)
         
         
--- pruebas.py
+++ pruebas.py
@@ -9,6 +9,7 @@ class TestMyModule(unittest.TestCase):
         a=(4,5)
         b=(4,6)
         self.assertEqual(Complejos.sumar(a, b), (8,11))
+        self.assertEqual(Complejos.sumar((3,-1), (1,4)), (4,3))
         
     def test_resta(self):
         a=(3,-2)
@@ -19,6 +20,8 @@ class TestMyModule(unittest.TestCase):
         a=(3,-2)
         b=(1,2)
         self.assertEqual(Complejos.multiplicar(a, b), (7,4))
+        self.assertEqual(Complejos.multiplicar((3,-1), (1,4)), (7,11))
+        self.assertEqual(Complejos.multiplicar((1,1), (-1,1)), (-2,0))
 
     def test_modulo(self):
         a=(1,-1)
@@ -35,7 +38,11 @@ class TestMyModule(unittest.TestCase):
 
     def test_cartesianoPolar(self):
         a=(1,1)
-        self.assertEqual(Complejos.cartesianoPolar(a[0],a[1]), (math.sqrt(2),math.pi/4))
+        self.assertEqual(Complejos.cartesianoPolar(a), (math.sqrt(2),math.pi/4))
+
+    def test_polarCartesiano(self):
+        a=(math.sqrt(2),math.pi/4)
+        self.assertEqual(Complejos.polarCartesiano(a),(1,1))        
 
         
 if __name__ == "__main__":
